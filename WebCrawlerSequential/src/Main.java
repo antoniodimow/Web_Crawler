a4671831aef39a59348ba32dbb5d018306ec1a22
antoniodimow <89221010@student.upr.si>
2025-12-05 11:20:25 +0100

    fetched the "https://www.famnit.upr.si/en/" HTML

--- WebCrawlerSequential/src/Main.java
+++ WebCrawlerSequential/src/Main.java
@@ -1,7 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
 
-    System.out.println("Hello World");
-
+public class Main {
+    public static void main(String[] args) throws Exception {
+        PageFetcher fetcher = new PageFetcher();
+        String html = fetcher.fetch("https://www.famnit.upr.si/en/");
+        System.out.println(html);
+    }
 }
---
+++ WebCrawlerSequential/src/PageFetcher.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class PageFetcher {
+    public String fetch(String url)throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+
+        HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+
+    }
+}
