6b14d451322786f8e419b0e9755d86789a0d62c4
antoniodimow <89221010@student.upr.si>
2025-12-05 11:44:51 +0100

    add link extraction from main page, that are same domain link from URL

--- WebCrawlerSequential/src/Main.java
+++ WebCrawlerSequential/src/Main.java
@@ -1,8 +1,30 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.net.URI;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        String url = "https://www.famnit.upr.si/en";
+        String domain = new URI(url).getHost();
+
         PageFetcher fetcher = new PageFetcher();
-        String html = fetcher.fetch("https://www.famnit.upr.si/en/");
-        System.out.println(html);
+        String html = fetcher.fetch(url);
+
+        Document doc = Jsoup.parse(html, url);
+        Elements links = doc.select("a[href]");
+
+        for (Element link : links) {
+
+            String href = link.attr("abs:href");
+
+            // if the domain is not the same, skip
+            if (!href.contains(domain)) {
+                continue;
+            }
+
+            System.out.println(href);
+        }
     }
 }
